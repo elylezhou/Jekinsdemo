027921fb35115ec4f12211bb54302c0c53ab56ab
elylezhou <294100445@qq.com>
2020-02-11 01:11:27 -0800

    first update

---
+++ CaculateLibrary.py
@@ -0,0 +1,15 @@
+"""
+Calculator library containing basic math operations.
+"""
+#encoding utf-8
+def add(first_term, second_term):
+    return first_term + second_term
+
+def subtract(first_term, second_term):
+    return first_term - second_term
+
+def multiplication(first_term, second_term):
+    return first_term * second_term
+
+def division(first_term, second_term):
+    return first_term / second_term
